2eb3dda5706fd0dd0f1e5f2cb9d3de18cd5af0d3
Bruno Meireles <bruno.meirelessilva90@gmail.com>
2025-03-16 16:57:05 -0300

    Adding modal

--- src/components/ProductModal.tsx/ProductModal.tsx
+++ src/components/ProductModal.tsx/ProductModal.tsx
@@ -1,6 +1,7 @@
-
+import { useState } from "react";
 import { ProductType } from "../../types/Product";
 import "./ProductModal.scss";
+import Button from "../ui/Button/Button";
 
 interface ProductModalProps {
   product: ProductType;
@@ -8,18 +9,44 @@ interface ProductModalProps {
 }
 
 const ProductModal: React.FC<ProductModalProps> = ({ product, onClose }) => {
+  
+  const [quantity, setQuantity] = useState(1);
+
+  const increment = () => setQuantity((prev) => prev + 1);
+  const decrement = () => setQuantity((prev) => (prev > 1 ? prev - 1 : 1));
+
   return (
-    <div className="modal">
-      <div className="modal-content">
+    <>
+      <div className="modal-overlay" onClick={onClose}></div>
+      <div className="modal">
         <span className="close" onClick={onClose}>
           &times;
         </span>
-        <img src={product.photo} alt={product.productName} />
-        <h3>{product.productName}</h3>
-        <p>{product.descriptionShort}</p>
-        <p className="modal__price">R$ {product.price}</p>
+        <div className="modal-container">
+          <img
+            src={product.photo}
+            alt={product.productName}
+            className="modal__image"
+          />
+          <div className="modal-content">
+            <h3 className="modal__title">{product.productName}</h3>
+            <p className="modal__price">R$ {product.price}</p>
+            <p className="modal__description">{product.descriptionShort}</p>
+            <a href="#" className="modal__details">
+              Veja mais detalhes do produto &gt;
+            </a>
+            <div className="modal__actions">
+              <div className="quantity-selector">
+                <button onClick={decrement}>-</button>
+                <span>{quantity}</span>
+                <button onClick={increment}>+</button>
+              </div>
+              <Button text="COMPRAR" onClick={() => {}}></Button>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
