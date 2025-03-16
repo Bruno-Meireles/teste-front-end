0f9dbff4ef6d28b87adca8a7f18685d7bdd8400b
Bruno Meireles <bruno.meirelessilva90@gmail.com>
2025-03-16 02:32:41 -0300

    work in Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,12 +5,35 @@ import search from "../../assets/icons/search.svg";
 import user from "../../assets/icons/user.svg";
 import logo from "../../assets/icons/logo.svg";
 import "./Header.scss";
+import { TopbarItem } from "../../types/ui";
+
+const topbarItems: TopbarItem[] = [
+  { icon: box, text: "Compra 100% segura" },
+  { icon: cart, text: "Frete grátis acima de R$ 200" },
+  { icon: heart, text: "Parcele suas compras" },
+];
+
 
 const Header = () => {
+    
+  
   return (
     <header className="header">
-      <div className="header__container">
+      <div className="header__topbar">
         <div className="content-1280">
+          <ul className="header__topbar-list">
+            {topbarItems.map((item, index) => (
+              <li key={index} className="header__topbar-item">
+                <img src={item.icon} alt={item.text} />
+                <span>{item.text}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      ;
+      <div className="header__container">
+        <div className="content-1280 space-between">
           <div className="header__logo">
             <img src={logo} alt="Econverse logo" />
           </div>
@@ -41,19 +64,20 @@ const Header = () => {
             </button>
           </div>
         </div>
+        <nav className="header__nav">
+          <div className="content-1141">
+            <ul className="header__menu">
+              <li className="header__menu-item"> TODAS CATEGORIAS</li>
+              <li className="header__menu-item">SUPERMERCADO</li>
+              <li className="header__menu-item">LIVROS</li>
+              <li className="header__menu-item">MODA</li>
+              <li className="header__menu-item">LANÇAMENTOS</li>
+              <li className="header__menu-item">OFERTAS DO DIA</li>
+              <li className="header__menu-item">ASSINATURA</li>
+            </ul>
+          </div>
+        </nav>
       </div>
-      {/* 
-      <nav className="header__nav">
-        <ul className="header__menu">
-          <li className="header__menu-item">Todas Categorias</li>
-          <li className="header__menu-item">Supermercado</li>
-          <li className="header__menu-item">Livros</li>
-          <li className="header__menu-item">Moda</li>
-          <li className="header__menu-item">Lançamentos</li>
-          <li className="header__menu-item">Ofertas do Dia</li>
-          <li className="header__menu-item">Assinatura</li>
-        </ul>
-      </nav> */}
     </header>
   );
 };
--- src/types/ui.tsx
+++ src/types/ui.tsx
@@ -2,3 +2,8 @@ export interface ButtonProps {
     text: string;
     onClick: () => void;
 }
+
+export interface TopbarItem {
+  icon: string;
+  text: string;
+}
